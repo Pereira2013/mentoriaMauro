0817db39e6c6a35fd1d208ec708cb0e8c3517dc2
robsonpereira <robson.pereira@sensedia.com>
2023-04-27 20:25:26 -0300

    feat: domain-professor

---
+++ src/main/java/br/com/projeto/mentoria/domain/Professor.java
@@ -0,0 +1,27 @@
+package br.com.projeto.mentoria.domain;
+
+public class Professor {
+    private int id;
+    private String name;
+    private String CPF;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+}
