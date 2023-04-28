269e25907f674b09954dc5761c2b29230cce7629
robsonpereira <robson.pereira@sensedia.com>
2023-04-28 08:25:56 -0300

    refactor: domain-professor

--- src/main/java/br/com/projeto/mentoria/domain/Professor.java
+++ src/main/java/br/com/projeto/mentoria/domain/Professor.java
@@ -1,5 +1,7 @@
 package br.com.projeto.mentoria.domain;
 
+import java.util.Objects;
+
 public class Professor {
     private int id;
     private String name;
@@ -8,7 +10,9 @@ public class Professor {
     public int getId() {
         return id;
     }
-
+    public void setId(int id) {
+        this.id = id;
+    }
     public String getName() {
         return name;
     }
@@ -23,5 +27,18 @@ public class Professor {
 
     public void setCPF(String CPF) {
         this.CPF = CPF;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Professor professor = (Professor) o;
+        return getId() == professor.getId() && Objects.equals(getName(), professor.getName()) && Objects.equals(getCPF(), professor.getCPF());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getCPF());
     }
 }
